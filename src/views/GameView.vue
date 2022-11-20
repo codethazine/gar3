a35ce7c9360bbe9d62a2f06a7f5f981508c1bd05
trebiscotty <luca.finocchiaro777@gmail.com>
2022-11-20 11:47:17 +0100

    created gaming

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,23 +1,20 @@
 <template>
-<div class="centered">
-<div id="square" class="square"></div>
-</div>
 <div class="header">
-<h1 style="font-size: 2.0rem;">GAR3</h1>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
-<button class="btn btn-outline-dark"></button>
+    <h1 class=Gar3 style="font-size: 2.1rem; margin: 0px;">GAR3</h1>
+    <button style="margin: 0px;" class="btn btn-outline-dark">0x71C7656EC7ab88b098defB751B7401B5f6d8976F</button>
 </div>
+<canvas>
 
+</canvas>
 </template>
 <script>
 </script>
 <style scoped>
 .square {
-	width:100px;
-	height:100px;
+	width:1100px;
+	height:500px;
 	background:black;
+    margin-top: 50px;
 }
 .centered {
     position: absolute;
@@ -26,11 +23,22 @@
     transform: translate(-50%, -50%);
 }
 .header {
-    position: absolute;
     font-family: 'Press Start 2P', cursive;
 }
-.btn btn-outline-dark {
-    position: top left;
-    
+.Gar3 {
+    left: 5px;
+    margin-top: 10px;
+}
+.btn {
+    right: 5px;
+    margin-top: 10px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+.header {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    align-content: center;
 }
 </style>
